55f57d1329f8368f58448022b4753fe98de77821
Luis Ramos <luismiguel.ramos@yahoo.com.au>
2022-04-19 16:43:57 +1000

    added routes on app.js and arrow functions on other files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage'
+import SignupPage from './pages/SignupPage/SignupPage'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Router>
+    {/* <Link to="/">Home</Link>
+    <Link to="/sign-up">Signup</Link>  */}
+    <Routes>
+      <Route path='/' element={<HomePage/>} />
+      <Route path='/sign-up' element={<SignupPage/>} /> 
+    </Routes>
+  </Router>
 }
 
-export default App;
+export default App
