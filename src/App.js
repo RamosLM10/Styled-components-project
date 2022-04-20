d3a4f997f195eaeb7b5641dd7200ee7f858a4bf2
Luis Ramos <luismiguel.ramos@yahoo.com.au>
2022-04-20 19:40:40 +1000

    worked on with all components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage'
 import SignupPage from './pages/SignupPage/SignupPage'
+import GlobalStyle from './globalStyles'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return <Router>
+    <GlobalStyle />
+    <Navbar></Navbar>
     {/* <Link to="/">Home</Link>
     <Link to="/sign-up">Signup</Link>  */}
     <Routes>
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Content from '../../components/Content/Content'
+import Footer from '../../components/Footer/Footer'
+import Hero from '../../components/Hero/Hero'
+import Pricing from '../../components/Pricing/Pricing'
+import { heroOne, heroTwo, heroThree } from '../../data/homeData'
 
 const HomePage = () => {
   return (
-    <div>Home</div>
+    <>
+      <Hero />
+      <Content {...heroOne}/>
+      <Content {...heroTwo}/>
+      <Content {...heroThree}/>
+      <Pricing />
+      <Footer />
+    </>
   )
 }
 
